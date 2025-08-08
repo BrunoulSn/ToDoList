917a72339034aca9a8e9ed466c7224eaf2822dc1
Bruno Cristiano <bruno120505@gmail.com>
2025-08-07 21:18:36 -0300

    Primeiro Commit

---
+++ Controllers/indexController.js
@@ -0,0 +1,12 @@
+function getToDoItems(){
+    let list_ToDoItem = localStorage.getItem()
+}
+
+function addToDoItem(){
+    let toDoItem = document.getElementById("ToDoItemName")
+}
+
+function removeItemToDoLost(){
+let toDoItem = document.getElementById("ToDoItemName")
+
+}
